7e335288d1232b2826f819b8e626118fbd6c7c7d
Mista-val <valerian.agbata@icloud.com>
2026-04-25 06:42:52 +0100

    google tag manager added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -74,6 +74,17 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KSKJPGGP');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5" />
         <meta name="google-site-verification" content="FkbCrqq9HGDcAVFk0SWHsRwL5J9vNtFKj5Np58_YgmY" />
         <meta name="google-adsense-account" content="ca-pub-1474614683509926" />
@@ -93,6 +104,14 @@ export default function RootLayout({
           crossOrigin="anonymous"></script>
       </head>
       <body className="min-h-full flex flex-col">
+        {/* Google Tag Manager (noscript) */}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KSKJPGGP"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
+        {/* End Google Tag Manager (noscript) */}
         <ThemeProvider>
           {children}
         </ThemeProvider>
